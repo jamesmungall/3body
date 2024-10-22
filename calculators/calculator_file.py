fda6a2d708d8f9933977c428a158d9dfb38dde6d
jamesmungall <preanuts26@gmail.com>
2024-10-22 11:20:39 +0100

    Add files via upload

---
+++ calculators/calculator_file.py
@@ -0,0 +1,15 @@
+class Calculator:
+    def get_deltas(self) -> dict:
+        pass
+
+    def get_params(self) -> [float]:
+        pass
+
+    def set_params(self, new_params):
+        pass
+
+    def get_names(self) -> [str]:
+        pass
+
+    def get_init_params(self) -> [float]:
+        pass
